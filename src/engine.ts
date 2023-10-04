ae01dcdf4c1281d55b6a12b367af0f5ecff71612
Xavier Cassel <xavier.cassel@zenika.com>
2023-10-04 12:09:45 +0200

    ♻️ refacto ts

--- src/engine.ts
+++ src/engine.ts
@@ -1,44 +1,47 @@
-import ejs from 'ejs';
-import fs from 'fs';
+import { render } from 'ejs';
+import type { Options } from 'ejs';
+import { cpSync, existsSync, mkdirSync, readFileSync, readdirSync, rmSync, writeFileSync } from 'node:fs';
+import type { Dirent } from 'node:fs'
 import MarkdownIt from 'markdown-it';
 import highlightjs from 'markdown-it-highlightjs';
-import { CheatsheetContext } from './cheatsheet-context.interface.ts';
+import type { CheatsheetContext } from './cheatsheet-context.interface.ts';
+import type { TemplateType } from './templates/template.type.ts';
 import { basicHeading } from './heading.rules.ts';
-import { TemplateType } from './templates/template.type.ts';
 
 const templatesDir: string = './src/templates/';
 const cheatsheetDir: string = './cheatsheets/';
 const outDir: string = './dist/';
-const ejsDefaultConfig: ejs.Options & { async: false } = { root: `${templatesDir}`, async: false };
+const ejsDefaultConfig: Options & { async: false } = { root: `${templatesDir}`, async: false };
 
 
 /**
  * Get list of available cheatsheets
  */
 export function getCheatsheetsList(): string[] {
-  return fs.readdirSync(cheatsheetDir, {withFileTypes: true})
-    .filter((dirent: any) => dirent.isDirectory())
-    .map((dir: any) => dir.name);
+  return readdirSync(cheatsheetDir, { withFileTypes: true })
+    .filter((dirent: Dirent) => dirent.isDirectory())
+    .map((dir: Dirent) => dir.name);
 }
 
 /**
  * Remove old dist folder and create new empty one
  */
 export function clearDist(): void {
-  fs.rmSync('./dist', { recursive: true, force: true });
-  fs.mkdirSync('./dist');
+  rmSync('./dist', { recursive: true, force: true });
+  mkdirSync('./dist');
 }
 
 /**
  * create template driven markdownit instance
  * @param cheatsheet name of the cheatsheet to build
  */
-export async function computeCheatsheet(cheatsheet: string): Promise<any> {
+export async function computeCheatsheet(cheatsheet: string): Promise<CheatsheetContext> {
   const markdown = new MarkdownIt({}).use(highlightjs);
 
   console.log(`${cheatsheet} cheatsheet.`);
+
   // load cheatsheet configuration
-  const config = JSON.parse(fs.readFileSync(`${cheatsheetDir}${cheatsheet}/config.json`).toString());
+  const config = JSON.parse(readFileSync(`${cheatsheetDir}${cheatsheet}/config.json`).toString());
 
   console.log(`Initialize ${config.template} template.`);
   // get template specific scripts and run it
@@ -48,7 +51,7 @@ export async function computeCheatsheet(cheatsheet: string): Promise<any> {
 
   console.log(`Render ${config.template} template.`);
   // render html body with markdownit
-  const rendered = markdown.render(fs.readFileSync(`${cheatsheetDir}${cheatsheet}/index.md`).toString());
+  const rendered = markdown.render(readFileSync(`${cheatsheetDir}${cheatsheet}/index.md`).toString());
 
   const context: CheatsheetContext = {
     cheatsheet,
@@ -62,32 +65,36 @@ export async function computeCheatsheet(cheatsheet: string): Promise<any> {
   };
 
   console.log(`Create ${config.template} HTML page.`);
+
   // create cheatsheet package with html page and assets
-  fs.mkdirSync(`${outDir}${cheatsheet}`);
-  const html: string = ejs.render(fs.readFileSync(`${templatesDir}${config.template}/index.ejs`).toString(), context, ejsDefaultConfig);
-  fs.writeFileSync(`${outDir}${cheatsheet}/index.html`, html);
-  fs.cpSync(`${templatesDir}${config.template}/style.css`, `${outDir}${cheatsheet}/style.css`);
-  if (fs.existsSync(`${cheatsheetDir}${cheatsheet}/assets`)) {
-    fs.cpSync(`${cheatsheetDir}${cheatsheet}/assets`, `${outDir}${cheatsheet}/assets`, { recursive: true });
+  mkdirSync(`${outDir}${cheatsheet}`);
+  const html: string = render(readFileSync(`${templatesDir}${config.template}/index.ejs`, { encoding: 'utf-8' }), context, ejsDefaultConfig);
+  writeFileSync(`${outDir}${cheatsheet}/index.html`, html);
+  cpSync(`${templatesDir}${config.template}/style.css`, `${outDir}${cheatsheet}/style.css`);
+
+  if (existsSync(`${cheatsheetDir}${cheatsheet}/assets`)) {
+    cpSync(`${cheatsheetDir}${cheatsheet}/assets`, `${outDir}${cheatsheet}/assets`, { recursive: true });
   }
 
   // copy common files
-  fs.cpSync(`${templatesDir}/template-common.css`, `${outDir}/template-common.css`);
+  cpSync(`${templatesDir}/template-common.css`, `${outDir}/template-common.css`);
   return context;
 }
 
-export function buildIndex(cheatsheetsContext: any[]): void {
-  const context: any = {
+export function buildIndex(cheatsheetsContext: CheatsheetContext[]): void {
+  const context = {
     cheatsheetsContext
   };
   console.log('Build index file');
-  fs.readdirSync(`${templatesDir}/main/`)
+
+  readdirSync(`${templatesDir}/main/`)
     .filter((dirent: string) => dirent.endsWith('.ejs'))
     .forEach((dirent: string) => {
-      const html: string = ejs.render(fs.readFileSync(`${templatesDir}/main/${dirent}`).toString(), context, ejsDefaultConfig);
-      fs.writeFileSync(`${outDir}/${dirent.substring(0, dirent.length - 3)}html`, html);
+      const html: string = render(readFileSync(`${templatesDir}/main/${dirent}`, { encoding: 'utf-8' }), context, ejsDefaultConfig);
+      writeFileSync(`${outDir}/${dirent.substring(0, dirent.length - 3)}html`, html);
     });
-  fs.readdirSync(`${templatesDir}/main/`)
+
+  readdirSync(`${templatesDir}/main/`)
     .filter((dirent: string) => !dirent.endsWith('.ejs'))
-    .forEach((dirent: string) => fs.cpSync(`${templatesDir}/main/${dirent}`, `${outDir}/${dirent}`));
+    .forEach((dirent: string) => cpSync(`${templatesDir}/main/${dirent}`, `${outDir}/${dirent}`));
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import type { CheatsheetContext } from './cheatsheet-context.interface.ts';
 import { buildIndex, clearDist, computeCheatsheet, getCheatsheetsList } from './engine.ts';
 
 clearDist();
 
-const cheatsheetsContext: any[] = [];
+const cheatsheetsContext: CheatsheetContext[] = [];
 const cheatsheets: string[] = getCheatsheetsList();
 for (let cheatsheet of cheatsheets) {
   cheatsheetsContext.push(await computeCheatsheet(cheatsheet));
